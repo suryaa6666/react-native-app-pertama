e0b6e2cb760c91d298994d37b9f8499de0dbfd21
suryaa6666 <suryaelidanto@gmail.com>
2021-09-29 17:23:09 +0700

    calling components with class

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
 import exampleImage from './assets/elzgamestudio.png';
 
@@ -7,14 +7,20 @@ const exampleImageUri = Image.resolveAssetSource(exampleImage).uri;
 const App = () => {
   return (
     <View>
-      <View style={{width: 1000, height: 70, backgroundColor: '#0984e3'}}>
+      <View style={{width: 1000, height: 70, backgroundColor: '#0984e3', marginBottom: 30}}>
         <Photo />
       </View>
+      <Profile />
       <Home />
       <Text> Cintara </Text>
       <Text> Surya </Text>
       <Text> Elidanto </Text>
-      <TextInput style={{borderWidth: 1}} placeholder={'masukkan nama...'} onTouchStart={() => console.log("menyentuh textinput username...")}/>
+      <TextInput
+        style={{borderWidth: 1}}
+        placeholder={'masukkan nama...'}
+        onTouchStart={() => console.log('menyentuh textinput username...')}
+      />
+      <KotakHijau />
     </View>
   );
 };
@@ -26,10 +32,43 @@ const Home = () => {
 const Photo = () => {
   return (
     <View>
-      <Image source={{uri: exampleImageUri}} style={{width: 140, height: 70}}  />
+      <Image source={{uri: exampleImageUri}} style={{width: 140, height: 70}} />
       {/* <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20}}> ELZGAME STUDIO </Text> */}
     </View>
   );
 };
+
+class KotakHijau extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'green',
+          alignItems: 'center',
+        }}>
+        <Text style={{color: 'white', marginTop: 25}}>
+          {' '}
+          Ini adalah kotak hijau!{' '}
+        </Text>
+      </View>
+    );
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Image
+          source={{uri: 'https://placeimg.com/100/100/people'}}
+          style={{width: 50, height: 50, borderRadius: 100}}
+        />
+        <Text> Cintara Surya Elidanto </Text>
+      </View>
+    );
+  }
+}
 
 export default App;
