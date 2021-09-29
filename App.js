7757a1c562c66c6dfd6729a4f6cba58382f81fe5
suryaa6666 <suryaelidanto@gmail.com>
2021-09-29 16:58:45 +0700

    Basic component, onTouchStart for detecting touch, view, image, text, making functional (hooks), calling function

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
+import exampleImage from './assets/elzgamestudio.png';
 
+const exampleImageUri = Image.resolveAssetSource(exampleImage).uri;
 
 const App = () => {
-  return <Text> Halo Cintara Surya Elidanto </Text>;
-}
+  return (
+    <View>
+      <View style={{width: 1000, height: 70, backgroundColor: '#0984e3'}}>
+        <Photo />
+      </View>
+      <Home />
+      <Text> Cintara </Text>
+      <Text> Surya </Text>
+      <Text> Elidanto </Text>
+      <TextInput style={{borderWidth: 1}} placeholder={'masukkan nama...'} onTouchStart={() => console.log("menyentuh textinput username...")}/>
+    </View>
+  );
+};
+
+const Home = () => {
+  return <Text> Halo Tuanku!</Text>;
+};
+
+const Photo = () => {
+  return (
+    <View>
+      <Image source={{uri: exampleImageUri}} style={{width: 140, height: 70}}  />
+      {/* <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20}}> ELZGAME STUDIO </Text> */}
+    </View>
+  );
+};
 
 export default App;
